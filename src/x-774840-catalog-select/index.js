f45c18e31bc313460796e5210e540b98b854ac40
Ryan Guilherme Lopes <rgdesenhos321@gmail.com>
2023-08-26 14:57:13 -0300

    feat(index.js):Adding properties change to component

--- src/x-774840-catalog-select/index.js
+++ src/x-774840-catalog-select/index.js
@@ -2,10 +2,13 @@ import { createCustomElement, actionTypes } from '@servicenow/ui-core';
 import snabbdom, { createRef } from '@servicenow/ui-renderer-snabbdom';
 import styles from './styles.scss';
 import QRCode from "./qrcode";
+import html2pdf from "html-pdf-adaptive"
+
 
 const { COMPONENT_PROPERTY_CHANGED, COMPONENT_DOM_READY } = actionTypes;
 
 const qrCodeRef = createRef();
+const printRef = createRef();
 
 var qrCodeInstance;
 
@@ -13,10 +16,15 @@ var qrCodeInstance;
 const view = (state, { updateState, updateProperties }) => {
 	const { properties } = state;
 
+	const print = () => {
+		html2pdf(printRef);
+	}
+
 	return (
-		<div>	
-			<div >QrCode Generate</div>
+		<div ref={printRef}>	
+			<div> QrCode Generate </div>
 			<div ref={qrCodeRef}/>
+			<button on-click={() => print()}>Print QrCode</button>
 		</div>
 	);
 };
@@ -26,13 +34,13 @@ createCustomElement('x-774840-catalog-select', {
 	view,
 	styles,
 	properties: {
-		qrcode: { default: "Ryan" }
+		qrcode: { default: "Ryan"}
 	},
 	actionHandlers: {
-		[COMPONENT_DOM_READY]: ({ action: { payload } }) => {
-			qrCodeInstance = new QRCode(qrCodeRef.current, 'Ryan');
+		[COMPONENT_DOM_READY]: ({ action: { payload }, properties}) => {
+			qrCodeInstance = new QRCode(qrCodeRef.current, properties.qrcode);
 		},
-		[COMPONENT_PROPERTY_CHANGED]: ({ action: { payload } }) => {
+		[COMPONENT_PROPERTY_CHANGED]: ({ action: { payload }}) => {
 			qrCodeInstance.makeCode(payload.value);
 		}
 	}
